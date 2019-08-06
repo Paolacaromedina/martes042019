a004636c4babb623e0d872ad423f835a221ad569
oswaldosilvafabra <osilvafabra@correo.unicordoba.edu.co>
2019-08-06 20:08:36 +0000

    cooc

--- bin/models/Personas.js
+++ bin/models/Personas.js
@@ -2,7 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PersonasSchema = new Schema({
-    personas: String,
+    nombre1: string,
+    nombre2: string,
+    apellido1: string,
+    apellido2: string,
+    telefono: string,
+    direccion: string,
+    sexo: string,
+    id_paises: {
+        type: Schema.Types.ObjectId,
+        ref: "Paises"
+    },
+    id_usuarios: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    id_usuarios:
 
 
 
